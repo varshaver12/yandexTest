c34eb17dee1efb7d2a31ae3508c4c5dcd771ff5e
khlestkinaleksey <khlestkinvarshaveraleksey@gmail.com>
2023-09-02 21:06:00 +0300

    Initial Commit

---
+++ yandexTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yandexTest
+//
+//  Created by 1234 on 02.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
